d32b9637bdb031dcb790ff14af4504b7d90c523e
Christian Kerl <christian-kerl@web.de>
2008-08-13 15:11:13 +0000

    [ckWebServicePlugin] replaced old wsdl generator with new ckWsdlGenerator in ckWebServiceWsdlBuildTask;

--- data/tasks/ckWebServiceWsdlBuildTask.php
+++ data/tasks/ckWebServiceWsdlBuildTask.php
@@ -44,21 +44,14 @@ function run_wsdl_build($task, $args)
   $root_modules_dir = sfConfig::get('sf_root_dir').'/apps/'.$sf_app.'/modules';
   $finder = pakeFinder::type('directory')->name('*')->relative()->maxdepth(0);
 
-  $ws_def = new WsdlDefinition();
-  $ws_def->setDefinitionName($ws_name);
-  $ws_def->setWsdlFileName(sfConfig::get('sf_root_dir').'/web/'.$ws_name.'.wsdl');
-  $ws_def->setNameSpace($ws_url);
-  $ws_def->setEndPoint($ws_url.$sf_controller.'.php');
-
-  $ws_write = new WsdlWriter($ws_def);
-
-  $methods = array();
+  $gen = new ckWsdlGenerator($ws_name, $ws_url, $ws_url.$sf_controller.'.php');
+  $gen->setCheckEnablement(true);
 
   foreach($finder->in($root_modules_dir) as $module_dir)
   {
     //proposed by Nicolas Martin to avoid problems with 'inited' modules
     if(!preg_match('/class(.*)Actions(.*)extends(.*)auto/', file_get_contents($root_modules_dir.'/'.$module_dir.'/actions/actions.class.php')) && file_exists($root_modules_dir.'/'.$module_dir.'/actions/actions.class.php'))
-    {       
+    {
       require_once($root_modules_dir.'/'.$module_dir.'/actions/actions.class.php');
 
       $class = new ReflectionClass($module_dir.'Actions');
@@ -88,12 +81,10 @@ function run_wsdl_build($task, $args)
           $action = ckString::lcfirst(substr($name, 7));
           $name = $module_dir.'_'.$action;
 
-          $param_return = _parse_method_comment($method->getDocComment());
-
-          if($param_return == null)
+          if(!$gen->addMethod($name, $method))
           {
             $yml[$sf_env][$action] = array('enable' => false);
-            
+
             continue;
           }
 
@@ -101,24 +92,10 @@ function run_wsdl_build($task, $args)
 
           $yml[$sf_env][$action] = array('enable'=>true, 'parameter'=>array(), 'result'=>null, 'render'=>false);
 
-          $ws_method = new WsdlMethod();
-          $ws_method->setName($name);
-
-          if(!is_null($param_return['return'] && !empty($param_return['return'])))
-          {
-            $ws_method->setReturn($param_return['return']['type'], $param_return['return']['desc']);
-          }
-
-          foreach($param_return['param'] as $param)
+          foreach(ckDocBlockParser::parseParameters($method->getDocComment()) as $param)
           {
             $yml[$sf_env][$action]['parameter'][] = $param['name'];
-
-            $ws_method->addParameter($param['type'], $param['name'], $param['desc']);
           }
-
-          $methods[] = $ws_method;
-
-          $ws_write->addMethod($ws_method);
         }
       }
 
@@ -128,20 +105,12 @@ function run_wsdl_build($task, $args)
         pake_echo_action('file+', $module_config);
         file_put_contents($module_config, sfYaml::dump($yml));
       }
-
     }
   }
 
-  $complexTypes = WsdlType::getComplexTypes($methods);
-
-  foreach ($complexTypes as &$complexType)
-  {
-    $ws_write->addComplexType($complexType);
-  }
-
-  $ws_write->doCreateWsdl();
-  $ws_write->save();
-  pake_echo_action('file+', $ws_def->getWsdlFileName());
+  $file = sprintf('%s/web/%s.wsdl', sfConfig::get('sf_root_dir'), $ws_name);
+  $def->save($file);
+  pake_echo_action('file+', $file);
 }
 
 function _register_lib_dirs()
@@ -164,49 +133,5 @@ function _register_lib_dirs()
   foreach($finder->in(sfConfig::get('sf_app_lib_dir')) as $dir)
   {
     simpleAutoLoader::register($dir, '.php');
-  }
-
-}
-
-function _parse_method_comment($comment)
-{
-  $result = array('param'=>array(), 'return'=>null);
-  $lines = explode("\n", $comment);
-
-  $enable = false;
-
-  foreach($lines as $line)
-  {
-    $line = trim($line);
-
-    if(ckString::startsWith($line, '* ') && substr($line, 2, 1) == '@')
-    {
-      $parts = explode(' ', substr($line, 3), 4);
-
-      if($parts[0] == 'ws-enable')
-      {
-        $enable = true;
-      }
-      else if($parts[0] == 'param' && count($parts)>=3)
-      {
-        $desc = isset($parts[3]) ? $parts[3] : '';
-        $result[$parts[0]][] = array('name'=>substr($parts[2], 1), 'type'=>$parts[1], 'desc'=>$desc);
-      }
-      else if($parts[0] == 'return' && count($parts)>=2)
-      {
-        $desc = isset($parts[2]) ? $parts[2] : '';
-        $desc .= isset($parts[3]) ? ' '.$parts[3] : '';
-        $result[$parts[0]] = array('type'=>$parts[1], 'desc'=>$desc);
-      }
-    }
-  }
-
-  if($enable)
-  {
-    return $result;
-  }
-  else
-  {
-    return null;
   }
 }
