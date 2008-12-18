1933b55c6111a13d218361f23be97b1d40ddcbba
Daniel Hürtgen <daniel@higidi.com>
2008-12-18 15:17:13 +0000

    ckSoapParameterFilter.class.php updated. Empty values mapped with a nullvalue to the requestclass, so you can use the fallback default value due the get. Also parameterlogging is possible, if sf_logging_enabled config var isset.

--- lib/ckSoapParameterFilter.class.php
+++ lib/ckSoapParameterFilter.class.php
@@ -18,33 +18,38 @@
  */
 class ckSoapParameterFilter extends sfFilter
 {
-  /**
+	/**
    * Executes this filter.
    *
    * @param sfFilterChain $filterChain A sfFilterChain instance
    */
-  public function execute($filterChain)
-  {
-    if($this->isFirstCall())
-    {
-      $request = $this->getContext()->getRequest();
-      $param   = $request->getParameter('ck_web_service_plugin.param', null);
-      $map     = sfConfig::get(sprintf('mod_%s_%s_parameter', strtolower($this->getContext()->getModuleName()), $this->getContext()->getActionName()));
+	public function execute($filterChain)
+	{
+		if($this->isFirstCall())
+		{
+			$request = $this->getContext()->getRequest();
+			$param   = $request->getParameter('ck_web_service_plugin.param', null);
+			$map     = sfConfig::get(sprintf('mod_%s_%s_parameter', strtolower($this->getContext()->getModuleName()), $this->getContext()->getActionName()));
 
-      if(is_array($param) && is_array($map))
-      {
-        $a = 0;
-        foreach($param as $index => $value)
-        {
-          if(count($map) > $a)
-          {
-            $request->setParameter($map[$a], $value);
-            $a++;
-          }
-        }
-      }
-    }
+			if(is_array($param) && is_array($map))
+			{
+				for ($i = 0; $i < count($map); $i++)
+				{
+					$request->setParameter($map[$i], !empty($param[$i]) ? $param[$i] : null);
 
-    $filterChain->execute();
-  }
+					if (sfConfig::get('sf_logging_enabled'))
+					{
+						$params[$map[$i]] = $param[$i];
+					}
+				}
+			}
+
+			if (sfConfig::get('sf_logging_enabled'))
+			{
+				$this->getContext()->getEventDispatcher()->notify(new sfEvent($this, 'application.log', array(sprintf('Mapped soap parameters due request %s', str_replace("\n", '', var_export($params, true))))));
+			}
+		}
+
+		$filterChain->execute();
+	}
 }
