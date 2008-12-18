6ff12f102f7e915667295d883d1c5a98a181b2f5
Daniel Hürtgen <daniel@higidi.com>
2008-12-18 15:54:26 +0000

    Bugfixes, replacing empty by if clause

--- lib/ckSoapParameterFilter.class.php
+++ lib/ckSoapParameterFilter.class.php
@@ -25,31 +25,52 @@ class ckSoapParameterFilter extends sfFilter
    */
 	public function execute($filterChain)
 	{
+		/** first execute of this chain **/
 		if($this->isFirstCall())
 		{
+			/** get request class from context **/
 			$request = $this->getContext()->getRequest();
-			$param   = $request->getParameter('ck_web_service_plugin.param', null);
-			$map     = sfConfig::get(sprintf('mod_%s_%s_parameter', strtolower($this->getContext()->getModuleName()), $this->getContext()->getActionName()));
 
+			/** get params from request **/
+			$param = $request->getParameter('ck_web_service_plugin.param', null);
+
+			/** get mapped params from mod config **/
+			$map = sfConfig::get(sprintf('mod_%s_%s_parameter', strtolower($this->getContext()->getModuleName()), $this->getContext()->getActionName()));
+
+			/** params and map is array **/
 			if(is_array($param) && is_array($map))
 			{
+				/** pass mapped params **/
 				for ($i = 0; $i < count($map); $i++)
 				{
-					$request->setParameter($map[$i], !empty($param[$i]) ? $param[$i] : null);
+					/** empty param **/
+					if ($param[$i] === '')
+					{
+						/** set to null **/
+						$param[$i] = null;
+					}
 
+					/** map soap param to request class **/
+					$request->setParameter($map[$i], $param[$i]);
+
+					/** logging enabled **/
 					if (sfConfig::get('sf_logging_enabled'))
 					{
+						/** append params **/
 						$params[$map[$i]] = $param[$i];
 					}
 				}
 			}
 
+			/** logging enabled **/
 			if (sfConfig::get('sf_logging_enabled'))
 			{
+				/** write logmessage **/
 				$this->getContext()->getEventDispatcher()->notify(new sfEvent($this, 'application.log', array(sprintf('Mapped soap parameters due request %s', str_replace("\n", '', var_export($params, true))))));
 			}
 		}
 
+		/** execute next filter **/
 		$filterChain->execute();
 	}
 }
