3d69074212e5951fdff28d5f29ee5260360d22d7
Christian Kerl <christian-kerl@web.de>
2009-01-10 15:21:22 +0000

    [ckWebServicePlugin] emptied trunk so development is only done on branches;

--- test/fixtures/project/apps/frontend/config/frontendConfiguration.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class frontendConfiguration extends sfApplicationConfiguration
-{
-  public function configure()
-  {
-    $this->getEventDispatcher()->connect('webservice.handle_header', array('SoapHeaderListener', 'listenToHandleHeader'));
-  }
-}
