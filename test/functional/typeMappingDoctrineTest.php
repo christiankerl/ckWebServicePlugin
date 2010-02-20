f3f5161b6e27e31141352d69195313604d7fd3d2
Christian Kerl <christian-kerl@web.de>
2010-02-20 14:02:31 +0000

    [ckWebServicePlugin] updated functional tests for doctrine/propel object mapping, so no longer object graphs with reference cycles are passed as parameters to the webservice; updated README;

--- test/functional/typeMappingDoctrineTest.php
+++ test/functional/typeMappingDoctrineTest.php
@@ -46,26 +46,60 @@ class myAuthor
     $Articles;
 }
 
-function checkFixtureModel(ckTestSoapClient $c)
+function checkFixtureModel(ckTestSoapClient $c, $withAuthor)
 {
   $c->isFaultEmpty()
     ->isCount('', 2)
     ->isType('', 'ckGenericArray')
     ->isType('0', 'myArticle')
+    ->is('0.id', 1)
+    ->is('0.title', 'Howto Build Webservices in Symfony')
+    ->is('0.content', 'Some Content.')
     ->isType('0.Authors', 'ckGenericArray')
-    ->isCount('0.Authors', 1)
-    ->isType('0.Authors.0', 'myAuthor')
     ->isType('0.Comments', 'ckGenericArray')
     ->isCount('0.Comments', 2)
     ->isType('0.Comments.0', 'myComment')
+    ->is('0.Comments.0.content', 'Physics is great!')
+    ->is('0.Comments.0.author', 'Isaac Newton')
+    ->isType('0.Comments.1', 'myComment')
+    ->is('0.Comments.1.content', 'I like cars!')
+    ->is('0.Comments.1.author', 'Henry Ford')
+    ->isType('1', 'myArticle')
+    ->is('1.id', 2)
+    ->is('1.title', 'Howto Write Tests In Symfony')
+    ->is('1.content', 'Some Content.')
+    ->isType('1.Authors', 'ckGenericArray')
+    ->isType('1.Comments', 'ckGenericArray')
+    ->isCount('1.Comments', 1)
+    ->isType('1.Comments.0', 'myComment')
+    ->is('1.Comments.0.content', 'Alles ist relativ!')
+    ->is('1.Comments.0.author', 'Albert Einstein')
   ;
 
   $result = $c->getResult();
 
   $t = $c->test();
-  $t->cmp_ok($result[0]->Authors[0], '===', $result[1]->Authors[0]);
   $t->cmp_ok($result[0]->Comments[0]->Article, '===', $result[0]->Comments[1]->Article);
   $t->cmp_ok($result[0], '===', $result[0]->Comments[0]->Article);
+
+  if($withAuthor)
+  {
+    $c
+    ->isCount('0.Authors', 1)
+    ->isType('0.Authors.0', 'myAuthor')
+    ->is('0.Authors.0.id', 1)
+    ->is('0.Authors.0.name', 'Christian Kerl')
+    ->isCount('1.Authors', 1)
+    ->isType('1.Authors.0', 'myAuthor')
+    ->is('1.Authors.0.id', 1)
+    ->is('1.Authors.0.name', 'Christian Kerl');
+
+    $t->cmp_ok($result[0]->Authors[0], '===', $result[1]->Authors[0]);
+    $t->cmp_ok($result[0], '===', $result[0]->Authors[0]->Articles[0]);
+    $t->cmp_ok($result[1], '===', $result[0]->Authors[0]->Articles[1]);
+    $t->cmp_ok($result[0], '===', $result[1]->Authors[0]->Articles[0]);
+    $t->cmp_ok($result[1], '===', $result[1]->Authors[0]->Articles[1]);
+  }
 }
 
 $_options = array(
@@ -81,5 +115,11 @@ $_options = array(
 
 $c = new ckTestSoapClient($_options);
 
-checkFixtureModel($c->getFixtureModel());
-checkFixtureModel($c->passFixtureModel($c->getResult()));
+checkFixtureModel($c->getFixtureModel(), true);
+
+$lastResult = $c->getResult();
+// unset Authors with cyclic reference causing problems during deserialization at server
+$lastResult[0]->Authors = array();
+$lastResult[1]->Authors = array();
+
+checkFixtureModel($c->passFixtureModel($lastResult), false);
--- test/functional/typeMappingPropelTest.php
+++ test/functional/typeMappingPropelTest.php
@@ -52,29 +52,62 @@ class myAuthor
     $name,
     $ArticleAuthors;
 }
-
-function checkFixtureModel(ckTestSoapClient $c)
+function checkFixtureModel(ckTestSoapClient $c, $withAuthor)
 {
-  $c->getFixtureModel()
-    ->isFaultEmpty()
+  $c->isFaultEmpty()
     ->isCount('', 2)
     ->isType('', 'ckGenericArray')
     ->isType('0', 'myArticle')
+    ->is('0.id', 1)
+    ->is('0.title', 'Howto Build Webservices in Symfony')
+    ->is('0.content', 'Some Content.')
     ->isType('0.ArticleAuthors', 'ckGenericArray')
-    ->isCount('0.ArticleAuthors', 1)
-    ->isType('0.ArticleAuthors.0', 'myArticleAuthor')
-    ->isType('0.ArticleAuthors.0.Author', 'myAuthor')
     ->isType('0.ArticleComments', 'ckGenericArray')
     ->isCount('0.ArticleComments', 2)
     ->isType('0.ArticleComments.0', 'myArticleComment')
-    ;
+    ->is('0.ArticleComments.0.content', 'Physics is great!')
+    ->is('0.ArticleComments.0.author', 'Isaac Newton')
+    ->isType('0.ArticleComments.1', 'myArticleComment')
+    ->is('0.ArticleComments.1.content', 'I like cars!')
+    ->is('0.ArticleComments.1.author', 'Henry Ford')
+    ->isType('1', 'myArticle')
+    ->is('1.id', 2)
+    ->is('1.title', 'Howto Write Tests In Symfony')
+    ->is('1.content', 'Some Content.')
+    ->isType('1.ArticleAuthors', 'ckGenericArray')
+    ->isType('1.ArticleComments', 'ckGenericArray')
+    ->isCount('1.ArticleComments', 1)
+    ->isType('1.ArticleComments.0', 'myArticleComment')
+    ->is('1.ArticleComments.0.content', 'Alles ist relativ!')
+    ->is('1.ArticleComments.0.author', 'Albert Einstein')
+  ;
 
   $result = $c->getResult();
 
   $t = $c->test();
-  $t->cmp_ok($result[0]->ArticleAuthors[0]->Author, '===', $result[1]->ArticleAuthors[0]->Author);
   $t->cmp_ok($result[0]->ArticleComments[0]->Article, '===', $result[0]->ArticleComments[1]->Article);
   $t->cmp_ok($result[0], '===', $result[0]->ArticleComments[0]->Article);
+
+  if($withAuthor)
+  {
+    $c
+    ->isCount('0.ArticleAuthors', 1)
+    ->isType('0.ArticleAuthors.0', 'myArticleAuthor')
+    ->isType('0.ArticleAuthors.0.Author', 'myAuthor')
+    ->is('0.ArticleAuthors.0.Author.id', 1)
+    ->is('0.ArticleAuthors.0.Author.name', 'Christian Kerl')
+    ->isCount('0.ArticleAuthors.0.Author.ArticleAuthors', 2)
+    ->isCount('1.ArticleAuthors', 1)
+    ->isType('1.ArticleAuthors.0', 'myArticleAuthor')
+    ->isType('1.ArticleAuthors.0.Author', 'myAuthor')
+    ->is('1.ArticleAuthors.0.Author.id', 1)
+    ->is('1.ArticleAuthors.0.Author.name', 'Christian Kerl')
+    ->isCount('1.ArticleAuthors.0.Author.ArticleAuthors', 2);
+
+    $t->ok($result[0]->ArticleAuthors[0]->Author === $result[1]->ArticleAuthors[0]->Author);
+    $t->ok($result[0] === $result[0]->ArticleAuthors[0]->Article);
+    $t->ok($result[1] === $result[1]->ArticleAuthors[0]->Article);
+  }
 }
 
 $_options = array(
@@ -92,5 +125,11 @@ $_options = array(
 
 $c = new ckTestSoapClient($_options);
 
-checkFixtureModel($c->getFixtureModel());
-checkFixtureModel($c->passFixtureModel($c->getResult()));
+checkFixtureModel($c->getFixtureModel(), true);
+
+$lastResult = $c->getResult();
+// unset ArticleAuthors with cyclic reference causing problems during deserialization at server
+$lastResult[0]->ArticleAuthors = array();
+$lastResult[1]->ArticleAuthors = array();
+
+checkFixtureModel($c->passFixtureModel($lastResult), false);
