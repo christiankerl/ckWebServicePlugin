202bb17fe87aefc4475bdd8fb8d6e45a1bf69560
Christian Kerl <christian-kerl@web.de>
2010-06-20 16:00:35 +0000

    [ckWebServicePlugin] set svn:keywords

--- test/fixtures/project/apps/frontend/modules/nonLC_name/actions/actions.class.php
+++ test/fixtures/project/apps/frontend/modules/nonLC_name/actions/actions.class.php
@@ -6,7 +6,7 @@
  * @package    project
  * @subpackage nonLC_name
  * @author     Christian Kerl <christian-kerl@web.de>
- * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ * @version    SVN: $Id$
  */
 class nonLC_nameActions extends sfActions
 {
